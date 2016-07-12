9d3b1865f4cfc52e3c8c432f9ac17ecb1496f67a
kanyarat <dokfaidokfai@gmail.com>
2016-07-12 16:42:44 +0700

    Finish

--- app/src/main/java/rus/kanyaratroungphueng/rusrun/SignUpActivity.java
+++ app/src/main/java/rus/kanyaratroungphueng/rusrun/SignUpActivity.java
@@ -32,6 +32,7 @@ public class SignUpActivity extends AppCompatActivity {
         avata4RadioButton = (RadioButton)findViewById(R.id.radioButton5);
 
 
+
     } // Main Method
 
     public void clickSignUpSign(View view){
