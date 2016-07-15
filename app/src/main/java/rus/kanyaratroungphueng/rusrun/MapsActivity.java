d2b488e42618e47fd805000602aed6bc1d32e4af
kanyarat <dokfaidokfai@gmail.com>
2016-07-15 16:19:41 +0700

    Finish 15July

--- app/src/main/java/rus/kanyaratroungphueng/rusrun/MapsActivity.java
+++ app/src/main/java/rus/kanyaratroungphueng/rusrun/MapsActivity.java
@@ -42,6 +42,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private LocationManager locationManager;
     private Criteria criteria;
     private double latUserADouble, lngUserADouble;
+    private boolean distanceABoolean = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +63,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
     }//Main Method
+    //นี่คือ เมทอด ที่หาระยะ ระหว่างจุด
+    private double distance(double lat1, double lon1, double lat2, double lon2) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1))
+                * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515 * 1.609344 * 1000;//meter Unit
+
+
+        return (dist);
+    }
+
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
+    }
 
     @Override
     protected void onResume() {
@@ -162,7 +183,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //Explicit
         private Context context;
         private GoogleMap googleMap;
-        private String urlJSON = "http://swiftcodingthai.com/rus/get_user_saikaew.php";
+        private String urlJSON = "http://swiftcodingthai.com/rus/get_user_master.php";
         private int[] avataInts = new int[]{R.drawable.bird48, R.drawable.doremon48,
                 R.drawable.kon48, R.drawable.nobita48, R.drawable.rat48};
 
@@ -218,8 +239,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 e.printStackTrace();
             }
-
-
         }//onPostExecute Method
     }//Create Marker Class
 
@@ -237,7 +256,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         CreateMarker createMarker = new CreateMarker(this, mMap);
         createMarker.execute();
 
+        //Check Distance
+        double latCheckPoint = 13.8587993;
+        double lngCheckPoint = 100.48220158;
+        double userDistance = distance(latCheckPoint, lngCheckPoint, latUserADouble, lngUserADouble);
+        Log.d("RusV5", "Distance ==>" + userDistance);
 
+        if (userDistance < 10) {
+            if (distanceABoolean) {
+                MyAlert myAlert = new MyAlert();
+                myAlert.myDialog(this,"ถึงฐานแล้ว","คุณเข้าใกล้ต่ำกว่า 10 เมตรแล้ว");
+                distanceABoolean = false;
+            }
+        }
         //Delay
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -254,7 +285,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private void editlatLngOnServer() {
 
-        String urlPHP = "http://swiftcodingthai.com/rus/edit_location_saikaew.php";
+        String urlPHP = "http://swiftcodingthai.com/rus/edit_location_master.php";
         OkHttpClient okHttpClient = new OkHttpClient();
         RequestBody requestBody = new FormEncodingBuilder()
                 .add("isAdd", "true")
