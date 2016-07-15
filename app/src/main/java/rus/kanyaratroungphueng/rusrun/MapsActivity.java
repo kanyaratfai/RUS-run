3733f2e71914b5393027c7e0e54264bbfb422169
kanyarat <dokfaidokfai@gmail.com>
2016-07-15 15:41:35 +0700

    Marker OK

--- app/src/main/java/rus/kanyaratroungphueng/rusrun/MapsActivity.java
+++ app/src/main/java/rus/kanyaratroungphueng/rusrun/MapsActivity.java
@@ -1,6 +1,7 @@
 package rus.kanyaratroungphueng.rusrun;
 
 import android.content.Context;
+import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
@@ -11,12 +12,14 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -34,8 +37,8 @@ import java.io.IOException;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private double latRusADouble = 13.837263;
-    private double lngARusDouble = 100.471980;
+    private double latRusADouble = 13.858962;
+    private double lngRusADouble = 100.482094;
     private LocationManager locationManager;
     private Criteria criteria;
     private double latUserADouble, lngUserADouble;
@@ -45,19 +48,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        //Set Location Service
+        //Setup Location Service
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);//สิ่งที่เห็นอาจอยู่ใน 300 เมตร
-        criteria.setAltitudeRequired(false); //ระยะห่าง ที่มีความสูงจากท้องทะเล
-        criteria.setBearingRequired(false);
+        criteria.setAccuracy(Criteria.ACCURACY_FINE); //ให้ทำการค้นหาอย่างละเอียด
+        criteria.setAltitudeRequired(false); //ถ้าตั้งเป็น true สามารถทำการหาค่าระดับน้ำทะเลได้ ถ้าตั้งเป็น false ต้องการแค่ค่า XY เท่านั้น
+        criteria.setBearingRequired(false);  //ถ้าตั้งเป็น true สามารถทำการหาค่าระดับน้ำทะเลได้ ถ้าตั้งเป็น false ต้องการแค่ค่า XY เท่านั้น
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-    }   // Main Method
+    }//Main Method
 
     @Override
     protected void onResume() {
@@ -66,67 +70,75 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         locationManager.removeUpdates(locationListener);
 
         latUserADouble = latRusADouble;
-        lngUserADouble = lngARusDouble;
+        lngUserADouble = lngRusADouble;
 
-        Location networkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
-        if (networkLocation != null) { //คือทำการหาพิกัดผ่านเน็ต จะถามตำแหน่งเน็ตโทรสับ ไม่เท่ากับความว่างเปล่า
+        Location networkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);//การหาพิกัดผ่าน network
+        if (networkLocation != null) {
+
             latUserADouble = networkLocation.getLatitude();
-            lngARusDouble = networkLocation.getAccuracy();
+            lngUserADouble = networkLocation.getLongitude();
         }
-        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);//ไม่มีเน็ตค้นหาได้
+
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);//หาพิกัดโดยไม่ต้องต่อเน็ต
         if (gpsLocation != null) {
             latUserADouble = gpsLocation.getLatitude();
-            lngUserADouble = gpsLocation.getLatitude();
+            lngUserADouble = gpsLocation.getLongitude();
         }
 
-
-
-
-    }   //onResume
+    }//onResume
 
     @Override
     protected void onStop() {
         super.onStop();
 
         locationManager.removeUpdates(locationListener);
-
     }
 
     public Location myFindLocation(String strProvider) {
+
         Location location = null;
-        if (locationManager.isProviderEnabled(strProvider)) {//ทำการเช็คว่าสัญญา เน็ตอยู่ในตำแหน่งไหน
-            locationManager.requestLocationUpdates(strProvider,100,10,locationListener);
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);//เลข 1000 คือหน่วยเวลาของ android เลข 10 คือถ้าขยับเกิน 10 เมตร
             location = locationManager.getLastKnownLocation(strProvider);
+
+
         } else {
-            Log.d("RusV2","Cannot Find Location");
-        }
-        return null;
-    }
-    //Create class ถ้าโทรสับเคลื่อนที่พิกัดก็จะทำงานเคลื่อนที่ตามโทรสับอัตโนมัติ
-    // *ถ้าไม่สามารถเชื่อมต่อเน็ตได้ ให้ใช้สัญญาของโทรสับแทน
+
+            Log.d("RusV2", "Cannot Find Location");
+
+        } //ไม่จริงทำงานที่นี่
+
+        return location;
+    }//จะทำงานและคืนค่า location กลับไป
+
+    //Create Class
     public LocationListener locationListener = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
+
             latUserADouble = location.getLatitude();
-            lngUserADouble = location.getLatitude();
-        }
+            lngUserADouble = location.getLongitude();
+
+
+        }//onLocation Change
 
         @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
+        public void onStatusChanged(String provider, int status, Bundle extras) {
 
         }
 
         @Override
-        public void onProviderDisabled(String s) {
+        public void onProviderEnabled(String provider) {
+
+        }//ถ้าสามารถทำงานกับ Internet ได้
+
+        @Override
+        public void onProviderDisabled(String provider) {
 
         }
-    };
-
+    };//listener ถ้าพิกัดมีการเปลี่ยนแปลงให้ทำงานอัตโนมัติ
 
 
 
@@ -134,98 +146,121 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        //Set Center Map
-        LatLng latLng = new LatLng(latRusADouble,lngARusDouble);
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));//ยิ่งตั้งค่าตัวเลขมากยิ่งใกล้มาก(latLng,...)
+        //Setup Center Map
+        LatLng latLng = new LatLng(latRusADouble, lngRusADouble);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+
         //Loop
         myLoop();
-        
-    }   //onMapReady
+
+
+    }//onMapReady
 
     private class CreateMarker extends AsyncTask<Void, Void, String> {
+
         //Explicit
         private Context context;
         private GoogleMap googleMap;
-        private String urlJSON = "http://swiftcodingthai.com/rus/get_user_master.php";
+        private String urlJSON = "http://swiftcodingthai.com/rus/get_user_saikaew.php";
+        private int[] avataInts = new int[]{R.drawable.bird48, R.drawable.doremon48,
+                R.drawable.kon48, R.drawable.nobita48, R.drawable.rat48};
 
-        private CreateMarker(Context context, GoogleMap googleMap) {
+        public CreateMarker(Context context, GoogleMap googleMap) {
             this.context = context;
             this.googleMap = googleMap;
-        } //Constructor
+        }//Constructor
 
         @Override
-        protected String doInBackground(Void... voids) {
+        protected String doInBackground(Void... params) {
 
             try {
+
                 OkHttpClient okHttpClient = new OkHttpClient();
                 Request.Builder builder = new Request.Builder();
                 Request request = builder.url(urlJSON).build();
                 Response response = okHttpClient.newCall(request).execute();
+
                 return response.body().string();
 
             } catch (Exception e) {
                 return null;
-            }
-
-        }//doInBack
+            }// try
+        }// DoInBack
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            Log.d("RusV4", "JSON==>" + s);
+            Log.d("RusV4", "JSON ==>>>" + s);
+
             try {
+
                 JSONArray jsonArray = new JSONArray(s);
                 for (int i=0;i<jsonArray.length();i++) {
+
                     JSONObject jsonObject = jsonArray.getJSONObject(i);
                     double douLat = Double.parseDouble(jsonObject.getString("Lat"));
                     double douLng = Double.parseDouble(jsonObject.getString("Lng"));
-                    String strName = jsonObject.getString("NAME");
+                    String strName = jsonObject.getString("Name");
+                    int intIndex = Integer.parseInt(jsonObject.getString("Avata"));
 
                     LatLng latLng = new LatLng(douLat, douLng);
                     googleMap.addMarker(new MarkerOptions()
                             .position(latLng)
-                            .title(strName));
+                            .title(strName)
+                            .icon(BitmapDescriptorFactory.fromResource(avataInts[intIndex])));
 
-                }   //for
+                }//for
+
+
+
             } catch (Exception e) {
+
                 e.printStackTrace();
             }
-        }   // onPost
-    }   //CreateMarker Class
+
+
+        }//onPostExecute Method
+    }//Create Marker Class
 
     private void myLoop() {
-        //To Do เพิ่มค่าตำแหน่ง
-        Log.d("RusV3", "latUser ==>" + latUserADouble);
-        Log.d("RusV3", "lnlUser ==>" + lngARusDouble);
-        //Edit Lat,Lng on Server
-        editLatLngOnServer();
+
+        //To Do
+        Log.d("RusV3", "latUser ==> " + latUserADouble);
+        Log.d("RusV3", "lngUser ==> " + lngUserADouble);
+
+        //edit Lat,Lng on server
+        editlatLngOnServer();
 
         //Create Marker
-
         mMap.clear();
         CreateMarker createMarker = new CreateMarker(this, mMap);
         createMarker.execute();
+
 
         //Delay
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                myLoop();
-            }
-        },3000);//การหน่วงเวลา3วินาที
-    }
 
-    private void editLatLngOnServer() {
-        String urlPHP = "http://swiftcodingthai.com/rus/edit_location_master.php";
-        String strID = getIntent().getStringExtra("LoginID");
-        Log.d("RusV3", "idUser==>" + strID);
+                myLoop();
+
+            }
+        }, 3000);
+
+
+    }//myLoop
+
+    private void editlatLngOnServer() {
+
+        String urlPHP = "http://swiftcodingthai.com/rus/edit_location_saikaew.php";
         OkHttpClient okHttpClient = new OkHttpClient();
         RequestBody requestBody = new FormEncodingBuilder()
                 .add("isAdd", "true")
                 .add("id", getIntent().getStringExtra("loginID"))
                 .add("Lat", Double.toString(latUserADouble))
-                .add("Lng", Double.toString(lngARusDouble))
+                .add("Lng", Double.toString(lngUserADouble))
                 .build();
         Request.Builder builder = new Request.Builder();
         Request request = builder.url(urlPHP).post(requestBody).build();
@@ -241,5 +276,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             }
         });
-    }
-}   //Main Class
+
+    }//editlatLngOnServer Method
+}//Main Class
